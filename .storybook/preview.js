d4e8635f918414b14e96c383f5b61d48488bc92c
supremeb <balachander.ral@gmail.com>
2025-05-12 11:41:00 -0700

    Fix: Import component CSS files in Storybook to properly display styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,13 @@
 // Import our design tokens and global styles
 import '../src/styles/main.css';
 
+// Import component CSS files
+import '../components/Button/Button.css';
+import '../components/Alert/Alert.css';
+import '../components/Card/Card.css';
+import '../components/Modal/Modal.css';
+import '../components/Badge/Badge.css';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
